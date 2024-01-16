754a84f7455e65203747248b7363a4bb0f884b25
dad3zero <dad3zero@gmail.com>
2024-01-16 14:53:26 +0100

    Added function to remove jpg from RAW+jpg

---
+++ lessons/photocleanup.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def remove_jpg_from_raw(dir_path:Path|str, missing_ok:bool=True):
+    photodir = Path(dir_path).resolve()
+
+    for file in photodir.glob('*.CR2'):
+        jpg_name = file.with_suffix(".JPG")
+
+        jpg_name.unlink(missing_ok=missing_ok)
