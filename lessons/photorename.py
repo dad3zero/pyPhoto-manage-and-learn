b953b9a5eae4d1ca915fc170452bac6f0f4d937e
dad3zero <dad3zero@gmail.com>
2024-01-16 09:55:12 +0100

    Updated with pathlib instead of old ways

--- lessons/photorename.py
+++ lessons/photorename.py
@@ -3,10 +3,11 @@ Program which should rename photo files according to their creation date.
 """
 
 import os
+from pathlib import Path
 import exifread
 from datetime import datetime
 
-image_ext = ['.jpg', '.jpeg', 'cr2', 'cr3' 'nef', 'dng']
+image_ext = ['.jpg', '.jpeg', '.cr2', '.cr3' '.nef', '.dng']
 
 
 def get_creation_date(image_file_path):
@@ -17,21 +18,26 @@ def get_creation_date(image_file_path):
                 tags.get('EXIF DateTimeOriginal')),
                 '%Y:%m:%d %H:%M:%S')
         except ValueError:
-            print("No value for ", image_file)
+            print("No value for ", image_file_path)
             return
 
 
-image_files = [ifile for ifile in os.listdir('.')
-               if os.path.splitext(ifile)[1].lower() in image_ext]
+image_paths = [ifile for ifile in Path('.').resolve().iterdir()
+               if ifile.suffix.lower() in image_ext]
 
-for image_file in image_files:
+for image_path in image_paths:
 
-    file_basename, file_ext = os.path.splitext(image_file)
+    file_basename, file_ext = os.path.splitext(image_path)
 
-    picture_date = get_creation_date(image_file)
+    picture_date = get_creation_date(image_path)
+
+    print(f'found {picture_date}')
 
     if picture_date:
-        rename_name = "/".join((os.path.dirname(image_file),
-                               picture_date.strftime('%y%m%d-%H%M%S') + file_ext))
-        if not os.path.exists(rename_name):
-            os.rename(image_file, rename_name)
+        renamed_file_name = picture_date.strftime('%y%m%d-%H%M%S') + image_path.suffix
+        rename_path = image_path.with_name(renamed_file_name)
+
+        try:
+            image_path.rename(rename_path)
+        except FileExistsError:
+            print(f"Fichier {rename_path} existe pour le fichier {image_path}")
